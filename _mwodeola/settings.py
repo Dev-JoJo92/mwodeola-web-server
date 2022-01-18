2caf5f11d71779917b6aae63c26058d50e8ff54b
CHOSEONGROK <csr8367@hanmail.net>
2022-01-18 16:29:43 +0900

    allowed host update

--- _mwodeola/settings.py
+++ _mwodeola/settings.py
@@ -44,7 +44,9 @@ SECRET_KEY_AES = get_secret("SECRET_KEY_AES")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com",
+]
 
 
 # Application definition
