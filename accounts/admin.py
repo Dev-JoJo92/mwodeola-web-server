f98be7d8bcd5bb6351687ecca03a999f1ca34671
CHOSEONGROK <csr8367@hanmail.net>
2022-01-19 19:08:30 +0900

    register model in django admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SNS, AccountGroup, AccountDetail, Account
 
 # Register your models here.
+admin.site.register(SNS)
+admin.site.register(AccountGroup)
+admin.site.register(AccountDetail)
+admin.site.register(Account)
